0e6102ae66171fd141c1101a874352b2e7db818c
Alloffices <alloffices.nyc@gmail.com>
2015-09-10 06:08:28 -0400

    add something

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,7 @@ class MessagesController < ApplicationController
   def create
     recipients = User.where(id: params['recipients'])
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
+    MessageMailer.current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).deliver_now
     flash[:success] = "Message has been sent!"
     redirect_to conversation_path(conversation)
   end
--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,2 +1,12 @@
 class MessageMailer < ActionMailer::Base
+
+  def send_message(recipients)
+
+  	@current_user = current_user
+  	@recipients = recipients
+
+  	mail(to: "#{recipients.email}",
+  		subject: "New project: #{current_user.email}"
+	)
+  end
 end
