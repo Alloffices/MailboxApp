1f4e7069199f5bbdd1e7628b5b6712925216c254
Alloffices <alloffices.nyc@gmail.com>
2015-09-10 11:47:20 -0400

    add new

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,6 +13,10 @@ class MessagesController < ApplicationController
     MessageMailer
     # Sending with mandrill
 
+
+
+
+
     flash[:success] = "Message has been sent!"
     redirect_to conversation_path(conversation)
   end
