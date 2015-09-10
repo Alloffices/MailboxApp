0afd7bda15be6194d7bf8b68f38dd2dca506128b
Alloffices <alloffices.nyc@gmail.com>
2015-09-10 03:46:00 -0400

    add mailer to conversation messages

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,8 +1,12 @@
 class MessageMailer < ActionMailer::Base
-  def new_message
-    mail(
-      to: recipient,
-      subject: message,
-    )
+  def new_message(current_user, conversation_user)
+
+	@current_user = current_user
+	@conversation_recipients = conversation_recipients
+
+    mail(to: conversation_recipients.email,
+    	subject: "Request From Another Creative",
+		body: "Hi, This is another request for session. #{current_user.name}, #{current_user.email}, #{current_user.skill}, #{current_user.location} "
+	)
   end
 end
